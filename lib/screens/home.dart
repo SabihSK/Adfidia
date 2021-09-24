d08b42d090bdd016b5505b1f7deda5ede856a1df
Sabih <sabih@thedatech.com>
2021-09-24 13:23:02 +0500

    first commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,9 +8,69 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
+Size getScreenSize(BuildContext context) => MediaQuery.of(context).size;
+
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold());
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 25),
+              child: Text(
+                "ADFIDIA",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.9),
+              child: Container(
+                height: 150,
+                width: 330,
+                child: Card(
+                    color: Colors.red,
+                    elevation: 8,
+                    child: Row(
+                      children: [
+                        Column(
+                          children: [
+                            Container(
+                                child: Text(
+                              "Wallet Balance",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )),
+                            Row(
+                              children: [
+                                Container(
+                                  child: Text(
+                                    "\$0",
+                                    style: TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
+                    )),
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
